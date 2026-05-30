8be5c3a4b754bde1617515a92fcf66d3d01007d4
Gyanaranjan Senapati <gyanaranjansenapati370@gmail.com>
2026-05-30 15:40:30 +0530

    added about but not wiorking

---
+++ src/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function About() {
+  return (
+   <div>
+    <p className="text-gray-600 leading-relaxed">
+  Welcome to our e-commerce store, your one-stop destination for quality
+  products at affordable prices. Explore a wide collection of fashion,
+  electronics, accessories, home essentials, and much more. We provide fast
+  delivery, secure payments, and trusted customer support to ensure the best
+  shopping experience for every customer.
+</p>
+</div>
+  )
+}
+
+export default About
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import Header from './Header.jsx'
 import Products from './Products.jsx'
+import About from './About.jsx'
+import { Routes, Route } from "react-router-dom";
 function App() {
   //state to manage search term in header component
   const [searchQuery, setSearchQuery] = useState("");
 
   return (
     <>
+    <Routes>
+      <Route pathe="/about" element={<About />} />
+      
+    </Routes>
     <Header  setSearchQuery={setSearchQuery} /> {/* passing setSearchTerm function as prop to Header component to update search term state in App component */}
      {searchQuery}
     <Products searchQuery={searchQuery}/>
