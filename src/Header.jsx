1754cf01d0cb9c81f7b0121aae0a87e2562365aa
Gyanaranjan Senapati <gyanaranjansenapati370@gmail.com>
2026-05-30 14:46:15 +0530

    final

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,21 +1,29 @@
 
-
+import './index.css'
 import {useState} from 'react'
 function Header( {setSearchQuery} ) {
-  // const [searchTerm, setSearchTerm] = useState("");
+  const [Search, setSearch] = useState("");
+  function handleSearch(){
+ setSearchQuery(Search);
+  }
+  function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+  console.log(document.body.className);
+}
 
   return (
    <>
    <header style={{display:"flex",alignItems:"center",justifyContent:"space-around",backgroundColor:"#f0f0f0",padding:"10px"}}>
     <img src="https://cdn-icons-png.flaticon.com/512/5968/5968705.png" alt="logo" className='logo' height="50" />
-    <input  onChange={(e)=>setSearchQuery(e.target.value)} type="text" placeholder='Search for products, brands and more' className='search' style={{border:"1px solid #ccc",borderRadius:"4px",padding:"5px" ,width:"30%"}}/>
-    <button  className='search-btn' style={{backgroundColor:"#007bff",color:"white",border:"none",borderRadius:"4px",padding:"5px 10px",cursor:"pointer"}}>Search</button>
+    <input  onChange={(e)=>setSearch(e.target.value)} type="text" placeholder='Search for products, brands and more' className='search' style={{border:"1px solid #ccc",borderRadius:"4px",padding:"5px" ,width:"30%"}}/>
+    <button onClick={handleSearch}  className='search-btn' style={{backgroundColor:"#007bff",color:"white",border:"none",borderRadius:"4px",padding:"5px 10px",cursor:"pointer"}}>Search</button>
     <nav style={{display:"flex",gap:"20px"}}>
       <a href="#"  style={{textDecoration:"none"}}>Home</a>
       <a href="#" style={{textDecoration:"none"}}>Products</a>
       <a href="#" style={{textDecoration:"none"}}>Contact</a>
       <a href="#" style={{textDecoration:"none"}}>About</a>
     </nav>
+    <button onClick={toggleDarkMode} className="LDMode">Toggle Dark Mode</button>
 
    </header>
   
